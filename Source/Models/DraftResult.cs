7bb3de8a11b2c14a1d0126fe9d7310e0112911eb
Ruby Blaire <rubyblaire.ffxiv@gmail.com>
2026-05-01 09:29:23 -0400

    Removing Unnecessary Items
    x

--- Source/Models/DraftResult.cs
+++
@@ -1,8 +0,0 @@
-namespace SceneKeeper.Models;
-
-public sealed class DraftResult
-{
-    public bool Success { get; init; }
-    public string Text { get; init; } = string.Empty;
-    public string Error { get; init; } = string.Empty;
-}
